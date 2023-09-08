9f751267cd9a9e969952bf27afbd741981e135f6
EC2 Default User <ec2-user@ip-172-31-13-251.ap-northeast-1.compute.internal>
2023-09-08 14:30:42 +0000

    Initial Commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,11 +9,12 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to books_path
+      redirect_to books_path(@book.id)
   end
 
   def show
     @book = Book.find(params[:id])
+    @user = current_user
   end
 
   def edit
@@ -24,5 +25,4 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :body, :image)
   end
-  
 end
